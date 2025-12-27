d8cb277f2ffc3a45ee6be5c4b42b4b9925a27424
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-12-27 14:49:36 +0100

    add volume calculation

---
+++ calc-volume.js
@@ -0,0 +1,47 @@
+let inputBox = document.getElementById("input");
+let outputBox = document.getElementById("output");
+let inputUnit = document.getElementById("inputSelect");
+let outputUnit = document.getElementById("outputSelect");
+
+function calculate() {
+  let input = inputBox.value;
+  let inUnit = inputUnit.value;
+  let outUnit = outputUnit.value;
+  let liter = getLiter(input, inUnit);
+  let output = getOutput(liter, outUnit);
+  outputBox.value = output;
+}
+
+function getLiter(input, unit) {
+  switch (unit) {
+    case "Liter":
+      return input;
+    case "Milliliter":
+      return input / 1000;
+    case "Cups":
+      return input / 4;
+    case "Tablespoon":
+      return input * 0.01478676;
+    case "Teaspoon":
+      return input * 0.004929;
+  }
+}
+
+function getOutput(input, unit) {
+  switch (unit) {
+    case "Liter":
+      return input;
+    case "Milliliter":
+      return input * 1000;
+    case "Cups":
+      return input * 4;
+    case "Tablespoon":
+      return input / 0.01478676;
+    case "Teaspoon":
+      return input / 0.004929;
+  }
+}
+
+inputBox.addEventListener("keyup", () => {
+  calculate();
+});
