ed50e50bb1c996ada9a5434454a78cfd60d545fd
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-12-27 14:19:48 +0100

    add length conversion

--- calc-length.js
+++ calc-length.js
@@ -1,12 +1,59 @@
 let inputBox = document.getElementById("input");
 let outputBox = document.getElementById("output");
+let inputUnit = document.getElementById("inputSelect");
+let outputUnit = document.getElementById("outputSelect");
+
+function calculate() {
+  let input = inputBox.value;
+  let inUnit = inputUnit.value;
+  let outUnit = outputUnit.value;
+  let meter = getMeters(input, inUnit);
+  let output = getOutput(meter, outUnit);
+  outputBox.value = Math.round(output * 100);
+}
+
+function getMeters(input, unit) {
+  switch (unit) {
+    case "Meter":
+      return input;
+    case "Centimeter":
+      return input / 100;
+    case "Millimeter":
+      return input / 1000;
+    case "Kilometer":
+      return input * 1000;
+    case "Miles":
+      return input * 1609.344;
+    case "Yards":
+      return input * 0.9144;
+    case "Feet":
+      return input * 0.3048;
+    case "Inches":
+      return input * 0.0254;
+  }
+}
+
+function getOutput(input, unit) {
+  switch (unit) {
+    case "Meter":
+      return input;
+    case "Centimeter":
+      return input * 100;
+    case "Millimeter":
+      return input * 1000;
+    case "Kilometer":
+      return input / 1000;
+    case "Miles":
+      return input / 1609.344;
+    case "Yards":
+      return input / 0.9144;
+    case "Feet":
+      return input / 0.3048;
+    case "Inches":
+      return input / 0.0254;
+  }
+}
 
 inputBox.addEventListener("keyup", () => {
   calculate();
 });
-
-function calculate() {
-  let input = inputBox.value;
-  console.log(input);
-  outputBox.value = input;
-}
